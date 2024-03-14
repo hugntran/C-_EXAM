fcb43484c6ebe4edb7b204a3b17e48d69031ded8
hugntran <133189997+hugntran@users.noreply.github.com>
2024-03-14 10:09:52 +0700

    Add files via upload

---
+++ GeometryExample/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GeometryExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Cylinder cylinder = new Cylinder(38.64, 22.48);
+            cylinder.Process();
+            cylinder.DisplayResult();
+        }
+    }
+}
